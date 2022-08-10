3bd953a5e47ae0c3cbbae4efbdd3868175c14d56
Burak Yıldız <developerburakyildiz@gmail.com>
2022-08-10 11:00:41 +0300

    review section added

--- js/script.js
+++ js/script.js
@@ -103,4 +103,28 @@ document.querySelector('.home').onmousemove = (e) =>{
       },
     },
   });
-    
+  var swiper = new Swiper(".review-slider", {
+    grabCursor: true,
+    centeredSlides: true,  
+    spaceBetween: 20,
+    loop:true,
+    autoplay: {
+      delay: 9500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
